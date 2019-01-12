5818c3a127f78f548aa05c78c3ba4c07153cb190
Divi Schmidt <divischmidt@gmail.com>
2019-01-11 17:56:35 -0700

    added function in order to check if the correct words are typed and added the results div to the page. data such as WPM is not being kept track of yet

--- assets/js/homescript.js
+++ assets/js/homescript.js
@@ -39,7 +39,6 @@ $(document).ready(function()  {
     function hexChanger(keyOpacity) {
         return event => {
             let keyName = event.key;
-            console.log(keyName);
 
             for (i = 0; i < currentKeyboard.length; i++) { 
                 for (n = 0; n < currentKeyboard[i].length; n++) {
@@ -58,9 +57,70 @@ $(document).ready(function()  {
     document.addEventListener('keydown', onKeyDown, false);
     document.addEventListener('keyup', onKeyUp, false);
 
-    
+    /* need to add timer functionality, add another function for eventlistening-but only fires once?
+        can use $('#typingbox').value to get whatever is entered into the textarea
+        needs to check value against the text in '#text p' and change color of word when incorrect
+        timer ends when the textarea reaches max number of characters.
 
-    
+        also needs functionality for multiple keyboards and uppercase letters
+        also maybe make the key opacity change for a set amount of time
+    */
 
+    const colorCurrentWord = "#dddddd";
+    const colorCorrectWord = "#93C572";
+    const colorIncorrectWord = "#e50000";
+
+    // test data
+    let wordData = {
+        seconds: 60,
+        correct: 0,
+        incorrect: 0,
+        total: 0,
+        typed: 0
+    };
+
+    // Changes the display value of an element from none to the onSetting. If not on none, changes it to none
+    function toggleDisplay(elemId, onSetting) {
+        if ($(elemId).css('display') === 'none') {
+            $(elemId).css('display', onSetting);
+        } else {
+            $(elemId).css('display', 'none');
+        }
+    } 
+
+    /* checks if the last character typed in the textarea is correct and updates the wordData accordingly.
+        also adds a red underline to all of the text if the letter typed is incorrect 
+        also checks to see if all of the text has been typed, if so, the results are displayed */
+    function checkLetter() {
+        let typed = document.querySelector('textarea').value;
+
+        let currentLetter = typed[typed.length - 1];
+        let correctText = $('#text p').text();
+
+        // display results after text is completed
+        if (typed.length === correctText.length) {
+            toggleDisplay('#resultbox', 'flex');
+            //TODO probably should do something else like remove the textarea
+        }
+
+        // check if last letter typed is correct
+        if (currentLetter !== correctText[typed.length - 1]) {
+            $('#typingbox').css('text-decoration', 'underline #F26D60');
+            //TODO needs to add data to the dictionary
+            console.log('wrong');
+        } else {
+            $('#typingbox').css('text-decoration', 'none');
+            //TODO needs to add data to the dictionary
+            console.log('right')
+        }
+    }
+
+    // adds the checkLetter function to the textbox as an event listener
+    var textArea = document.querySelector('textarea');
+    textArea.addEventListener('input', checkLetter, false); 
+    
+    
+    //shows result box at the moment but should show options- not in html yet    
+    $("#options").click(function () { toggleDisplay('#resultbox', 'flex')});
 
 });
